3ae393d1e380c7554e5ddf628961e1de6f1d2cf6
Lourrayni <lourraynidantass@gmail.com>
2025-11-06 19:55:57 -0300

    feat: criando  models e migrations para User e Tarefa. Configura SoftDeletes, casts e fillable no Model Tarefa. adicionando user_id (FK) na tabela tarefas.

---
+++ database/seeders/TarefaSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Tarefa;
+use Illuminate\Support\Facades\Hash;
+
+class TarefaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user1 = User::factory()->create([
+            'name' => 'Utilizador Teste',
+            'email' => 'teste@email.com',
+            'password' => Hash::make('senha123'),
+        ]);
+
+        Tarefa::factory()->count(15)->create([
+            'user_id' => $user1->id,
+            'status' => false
+        ]);
+        Tarefa::factory()->count(10)->create([
+            'user_id' => $user1->id,
+            'status' => true
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'Outro Utilizador',
+            'email' => 'outro@email.com',
+            'password' => Hash::make('senha123'),
+        ]);
+
+        Tarefa::factory()->count(5)->create([
+            'user_id' => $user2->id,
+            'status' => false
+        ]);
+    }
+}
